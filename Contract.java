ca283a60437aac1bdddbba0237ec9ef548952e28
HNDThuan <thuan.huynhnguyen.cit20@eiu.edu.vn>
2025-01-09 15:59:27 +0700

    add tenant, propertyOwner, contract classes

---
+++ Contract.java
@@ -0,0 +1,14 @@
+public class Contract {
+    String contractID;
+    String tenantID;
+     String propertyID;
+     double rentAmount;
+    public Contract(String contractID, String tenantID, String propertyID, double rentAmount) {
+        this.contractID = contractID;
+        this.tenantID = tenantID;
+        this.propertyID = propertyID;
+        this.rentAmount = rentAmount;
+    }
+
+     
+}
---
+++ PropertyOwner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PropertyOwner {
+    int userID;
+    List<Integer> listOfProperties = new ArrayList<>();
+
+    public PropertyOwner(int userID) {
+        this.userID = userID;
+    }
+
+}
